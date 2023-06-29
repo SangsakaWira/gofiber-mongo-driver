2e48652d29e5b759d67b151049d18f899fe608f4
Sangsaka Wira <sangsakawira@Sangsakas-MacBook-Pro.local>
2023-06-29 20:37:31 +0700

    complete basic crud

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ import "github.com/gofiber/fiber/v2"
 import (
     "context"
     "log"
-    "fmt"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
     "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var ctx = context.Background()
 
-type book struct {
-    judul  string `bson:"judul"`
-    harga string    `bson:"harga"`
-    deskripsi string    `bson:"deskripsi"`
+type Book struct {
+    Judul  string `bson:"judul"`
+    Harga string    `bson:"harga"`
+    Deskripsi string    `bson:"deskripsi"`
+}
+
+type BookUpdate struct {
+    ID string `bson:"id"`
+    Judul  string `bson:"judul"`
+    Harga string    `bson:"harga"`
+    Deskripsi string    `bson:"deskripsi"`
 }
 
 func connect() (*mongo.Database, error) {
@@ -34,86 +41,146 @@ func connect() (*mongo.Database, error) {
     return client.Database("libnation-back-end"), nil
 }
 
-func find() {
-    db, err := connect()
-    if err != nil {
-        log.Fatal(err.Error())
-    }
+func main() {
+    app := fiber.New()
 
-    csr, err := db.Collection("books").Find(ctx, bson.M{"judul": "Harry Potter: The Chamber of Secret"})
-    if err != nil {
-        log.Fatal(err.Error())
-    }
-    defer csr.Close(ctx)
+    app.Post("/", func(c *fiber.Ctx) error {
 
-    result := make([]book, 0)
-    for csr.Next(ctx) {
-        var row book
-        err := csr.Decode(&row)
+        db, err := connect()
         if err != nil {
             log.Fatal(err.Error())
         }
 
-        result = append(result, row)
-    }
+        // Define a struct to match the JSON structure
+		var book Book
 
-    if len(result) > 0 {
-        fmt.Println("judul  :", result[0].judul)
-        fmt.Println("harga :", result[0].harga)
-    }
-}
-
-
-func main() {
-    app := fiber.New()
-
-    app.Get("/", func(c *fiber.Ctx) error {
-        // Set up the MongoDB connection
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Ping the MongoDB server to verify the connection
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Access the MongoDB database and collection
-	database := client.Database("libnation-back-end")
-	collection := database.Collection("books")
-
-    // Find all documents in the collection
-	cursor, err := collection.Find(context.Background(), bson.D{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer cursor.Close(context.Background())
-
-	// Create a slice to hold the retrieved documents
-	var documents []bson.M
-
-	// Iterate over the cursor to retrieve documents
-	for cursor.Next(context.Background()) {
-		var doc bson.M
-		if err := cursor.Decode(&doc); err != nil {
+		// Parse the JSON request body into the struct
+		if err := c.BodyParser(&book); err != nil {
 			log.Fatal(err)
 		}
-		// Append each document to the slice
-		documents = append(documents, doc)
-	}
 
-	// Check if any errors occurred during iteration
-	if err := cursor.Err(); err != nil {
-		log.Fatal(err)
-	}
+        _, err = db.Collection("books").InsertOne(ctx,Book{book.Judul,book.Harga,book.Deskripsi})
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+    
+        return c.Send(c.Body()) // []byte("user=john")
+    })
 
-	// Process the retrieved documents as needed
-	// For example, you can return them as JSON response
-	return c.JSON(documents)
+    app.Get("/", func(c *fiber.Ctx) error {
+        db, err := connect()
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+
+        cursor, err := db.Collection("books").Find(context.Background(), bson.D{})
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        defer cursor.Close(context.Background())
+
+        var documents []bson.M
+
+        for cursor.Next(context.Background()) {
+            var doc bson.M
+            if err := cursor.Decode(&doc); err != nil {
+                log.Fatal(err)
+            }
+            documents = append(documents, doc)
+        }
+
+        if err := cursor.Err(); err != nil {
+            log.Fatal(err)
+        }
+
+        return c.JSON(documents)
+    })
+
+    app.Get("/:id", func(c *fiber.Ctx) error {
+        db, err := connect()
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+
+        collection := db.Collection("books")
+
+        id := c.Params("id")
+
+        objID, _ := primitive.ObjectIDFromHex(id)
+
+        cursor, err := collection.Find(context.Background(), bson.M{"_id":objID})
+
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        defer cursor.Close(context.Background())
+
+        var documents []bson.M
+
+        for cursor.Next(context.Background()) {
+            var doc bson.M
+            if err := cursor.Decode(&doc); err != nil {
+                log.Fatal(err)
+            }
+            documents = append(documents, doc)
+        }
+
+        if err := cursor.Err(); err != nil {
+            log.Fatal(err)
+        }
+
+        return c.JSON(documents)
+    })
+
+    app.Patch("/", func(c *fiber.Ctx) error {
+
+        db, err := connect()
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+
+        // Define a struct to match the JSON structure
+		var book BookUpdate
+
+		// Parse the JSON request body into the struct
+		if err := c.BodyParser(&book); err != nil {
+			log.Fatal(err)
+		}
+
+        objID, _ := primitive.ObjectIDFromHex(book.ID)
+
+        var selector = bson.M{"_id": objID}
+
+        _, err = db.Collection("books").UpdateOne(ctx,selector,bson.M{"$set":Book{book.Judul,book.Harga,book.Deskripsi}})
+        
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+    
+        return c.SendString("Successfully Update Data!") //
+    })
+
+    app.Delete("/:id", func(c *fiber.Ctx) error {
+        db, err := connect()
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+
+        collection := db.Collection("books")
+
+        id := c.Params("id")
+
+        objID, _ := primitive.ObjectIDFromHex(id)
+
+        _, err = collection.DeleteOne(context.Background(), bson.M{"_id":objID})
+
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        return c.SendString("Remove success!")
     })
 
     app.Listen(":3000")
